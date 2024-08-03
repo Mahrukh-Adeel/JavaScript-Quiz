479dae5dab7a26bafec50f870b61d83b84c62417
Mahrukh Adeel <150025532+Mahrukh-Adeel@users.noreply.github.com>
2024-08-03 10:20:32 +0000

    modified

--- objects.js
+++ objects.js
@@ -18,5 +18,65 @@ const print =()=>(
     console.log(`Occupation: ${person.occupation}`)
 );
 print()
+const VsUser ={
+    name:"M",
+    location: "isl",
+    loggedIn: false
+}
+//Write a function to clone an object.
+const clone = (person) => {
+    return Object.assign({}, person);
+};
+let cloned=clone(person)
+console.log(cloned)
 
-//
+//Write a function to merge two objects.
+console.log("Merged")
+const merge =(person, obj)=>{
+    return Object.assign({},person, obj);
+}
+let merged= merge(person, VsUser)
+console.log(merged)
+
+//Write a function that takes an object and returns an array of its keys.
+const keys = (merged) =>{
+    return Object.keys(merged);
+}
+console.log(keys(merged))
+
+//Write a function that takes an object and returns an array of its values.
+const values = (merged) =>{
+    return Object.values(merged);
+}
+console.log(values(merged))
+
+//Write a function that takes an object and returns an array of key-value pairs.
+const pairs = (merged) =>{
+    return Object.entries(merged);
+}
+console.log(pairs(merged))
+
+// Write a function to deeply merge two nested objects.
+const regUser={
+    email:"1bc@mail.com",
+    fName: {
+        fname: "m",
+        lname: "a"
+    }
+}
+console.log("Deep Merged")
+const deepMerge = (obj1, obj2) => {
+    const deep = { ...obj1 }; 
+    for (const key of Object.keys(obj2)) {
+        if (obj2[key] instanceof Object && obj1[key] instanceof Object) {
+            deep[key] = deepMerge(obj1[key], obj2[key]); 
+        } else {
+            deep[key] = obj2[key]; 
+        }
+    }
+    return deep;
+};
+let deepM= deepMerge(VsUser, regUser)
+console.log(deepM)
+
+//Write a function to update the properties of an object using another object.
